openshift/kubernetes-cli-runtime

UPSTREAM: <carry>: kubectl: allow --config and --kubeconfig
Kubernetes-commit: fd609bbc36adfbb830dc860f373eca1adf806ed2

pkg/genericclioptions/patch_config_flags.go
@@ -1,0 +1,16 @@
+package genericclioptions
+
+import "fmt"
+
+// OpenShiftKubeConfigFlagName exists only so that we can track down all non-standard offenders
+const OpenShiftKubeConfigFlagName = "config"
+
+func NewErrConfigurationMissing() error {
+	return fmt.Errorf(`Missing or incomplete configuration info.  Please login or point to an existing, complete config file:
+
+  1. Via the command-line flag --config
+  2. Via the KUBECONFIG environment variable
+  3. In your home directory as ~/.kube/config
+
+To view or setup config directly use the 'config' command.`)
+}

soltysh/cli-runtime

Kubernetes-commit: fb4d9bd7de45e5a46cdd9acdd61f5410b5d45bf5

pkg/genericclioptions/patch_config_flags.go
@@ -1,0 +1,16 @@
+package genericclioptions
+
+import "fmt"
+
+// OpenShiftKubeConfigFlagName exists only so that we can track down all non-standard offenders
+const OpenShiftKubeConfigFlagName = "config"
+
+func NewErrConfigurationMissing() error {
+	return fmt.Errorf(`Missing or incomplete configuration info.  Please login or point to an existing, complete config file:
+
+  1. Via the command-line flag --kubeconfig
+  2. Via the KUBECONFIG environment variable
+  3. In your home directory as ~/.kube/config
+
+To view or setup config directly use the 'config' command.`)
+}